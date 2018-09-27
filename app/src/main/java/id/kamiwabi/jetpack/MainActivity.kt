7c44a7a316514af46f4d84f1ce1e477b42fd3702
G9 <2202insane@gmail.com>
2018-09-27 21:04:16 +0700

    add basic implicit manifest navigation

---
+++ app/src/main/java/id/kamiwabi/jetpack/MainActivity.kt
@@ -0,0 +1,13 @@
+package id.kamiwabi.jetpack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
---
+++ app/src/main/java/id/kamiwabi/jetpack/ui/main/MainViewModel.kt
@@ -0,0 +1,15 @@
+package id.kamiwabi.jetpack.ui.main
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val _data = MutableLiveData<String>()
+    val data: LiveData<String>
+        get() = _data
+
+    init {
+        _data.value = "Hello Wabi"
+    }
+}
