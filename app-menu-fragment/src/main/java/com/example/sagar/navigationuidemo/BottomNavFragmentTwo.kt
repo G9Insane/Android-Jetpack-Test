e08ac01d3290a8bd2e7f71b94159ac9de01bc0b8
G9 <2202insane@gmail.com>
2018-09-28 20:39:44 +0700

    app-menu-fragment module

---
+++ app-menu-fragment/src/main/java/com/example/sagar/navigationuidemo/BottomNavFragmentTwo.kt
@@ -0,0 +1,20 @@
+package com.example.sagar.navigationuidemo
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class BottomNavFragmentTwo : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_nav_fragment_two, container, false)
+    }
+}
