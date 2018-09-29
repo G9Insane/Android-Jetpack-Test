66c17eb4b219ee916d5a9ac51e92959387674a0f
G9 <2202insane@gmail.com>
2018-09-30 02:36:38 +0700

    i think sould buy new laptop for more productive

--- jetpack/src/main/java/com/example/jetpack/ui/main/SecondFragment.kt
+++ jetpack/src/main/java/com/example/jetpack/ui/main/SecondFragment.kt
@@ -18,16 +18,12 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
-class MainFragment : Fragment() {
-
-//    companion object {
-//        fun newInstance() = MainViewModel
-//    }
+class SecondFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
 
