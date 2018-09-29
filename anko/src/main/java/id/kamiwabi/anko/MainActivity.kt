618c204775dd6cde6307bd3354986e4d0960accb
G9 <2202insane@gmail.com>
2018-09-29 19:10:12 +0700

    tested button with anko dsl

--- anko/src/main/java/id/kamiwabi/anko/MainActivity.kt
+++ anko/src/main/java/id/kamiwabi/anko/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import org.jetbrains.anko.*
-
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        verticalLayout { //Start a Layout (verticalLayout,linearLayout,relativeLayout...)
+            linearLayout { //Inside one, you can start another one
+                //Content of layout, like buttons and other things
+                button("Some text"){
+                    onClick { toast("Hi there!") }
+                }
+            }
+        }
         toast("Hi there!")
         toast(R.string.message)
         longToast("Wow, such duration")
