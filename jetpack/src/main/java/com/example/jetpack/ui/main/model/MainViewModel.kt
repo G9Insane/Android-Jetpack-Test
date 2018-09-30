539d0531a1ae5b976622ba3f633822bfbf793781
G9 <2202insane@gmail.com>
2018-09-30 23:52:54 +0700

    get some rest for while

--- jetpack/src/main/java/com/example/jetpack/ui/main/model/MainViewModel.kt
+++ jetpack/src/main/java/com/example/jetpack/ui/main/model/MainViewModel.kt
@@ -1,2 +1,14 @@
 package com.example.jetpack.ui.main.model
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    data class User(val firstName: String, val lastName: String)
+
+    private val _user = MutableLiveData<User>()
+
+    val user: LiveData<User> get() = _user
+
+}
