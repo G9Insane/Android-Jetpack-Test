a6c8abdbba8a512aee8e6a8854b346c8fd8a240f
Galang <2202insane@gmail.com>
2018-10-09 21:28:54 +0700

    example icon launcher

--- jetpack/src/main/java/com/example/jetpack/ui/main/HomeFragment.kt
+++ jetpack/src/main/java/com/example/jetpack/ui/main/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.example.jetpack.ui.main
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 
 import com.example.jetpack.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +27,14 @@ class HomeFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnHome.setOnClickListener {
+            view.findNavController().navigate(R.id.aboutFragment)
+        }
     }
 
 
