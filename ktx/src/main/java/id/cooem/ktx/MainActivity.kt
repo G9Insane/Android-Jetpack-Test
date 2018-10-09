63b491f11bcfe9d698c82ca0d832250eda30e3cf
Galang <2202insane@gmail.com>
2018-10-09 22:12:21 +0700

    add missing lib

---
+++ ktx/src/main/java/id/cooem/ktx/MainActivity.kt
@@ -0,0 +1,22 @@
+package id.cooem.ktx
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.main_activity.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
